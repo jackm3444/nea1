423e365d1d090461b3b944dc6916712da4ed7ccb
JackMills344 <108814455+JackMills344@users.noreply.github.com>
2023-10-20 22:21:08 +0000

    Tested MongoDB

---
+++ database/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', true);
+const { MONGODB_URL } = process.env
+
+exports.connect = () => {
+    console.log(typeof MONGODB_URL);
+    mongoose.connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(
+        console.log("DB connected successfully")
+    )
+    .catch((error) => {
+        console.log(`DB connection FAILED`);
+        console.log(error);
+        process.exit(1)
+    })
+}
