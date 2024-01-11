ce01929695be0e2e31343516975cc74e60f83078
jackm3444 <156239853+jackm3444@users.noreply.github.com>
2024-01-11 22:01:54 +0000

    changes

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const app = require('./app')
-const {PORT} = process.env
-app.listen(PORT, () => {
-    console.log(`SERVER is running at port: ${PORT}`);
-})
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+    
+  });
+}).listen(8080);
